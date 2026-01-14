e4d97ba55e48a9aa73d35361525b9e0093047e35
amit kumar <amitarya95080@gmail.com>
2026-01-14 23:31:25 +0530

    Generate random color using js.

--- GenerateTheRandomColor/script.js
+++ GenerateTheRandomColor/script.js
@@ -1,15 +1,15 @@
 let btn = document.querySelector("button");
 
-
 btn.addEventListener("click", function(){
-    let randomColor = randomColor();
-    document.querySelector(".box").style.backgroundColor = "#" + randomColor;
-    document.querySelector("input").value = "#" + randomColor;
+    let rdmColor = randomColor();
+    document.querySelector(".box").style.backgroundColor =rdmColor;
+    document.querySelector("input").value = rdmColor;
 })
 
 function randomColor(){
-    let r=Math.floor(Math.random()*256).toString(16).padStart(2,'0');
-    let g=Math.floor(Math.random()*256).toString(16).padStart(2,'0');
-    let b=Math.floor(Math.random()*256).toString(16).padStart(2,'0');
-    return "rgb"+"("+"#" + r + " , " + g + " , " + b+" )";
+    let r=("000"+Math.floor(Math.random()*256).toString()).slice(-3);
+    let g=("000"+Math.floor(Math.random()*256).toString()).slice(-3);
+    let b=("000"+Math.floor(Math.random()*256).toString()).slice(-3);
+
+    return "rgb"+"("+r+","+g+","+b+")";
 }
